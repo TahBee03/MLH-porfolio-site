4ee49ed4b0ef073c8e3cc9ad6c92c70d3d6ecec2
Talike <talikeb20@gmail.com>
2022-07-04 01:26:34 -0400

    revised redeploy_site.sh

--- redeploy_site.sh
+++ redeploy_site.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 
-cd MLH-portfolio-site
 git fetch && git reset origin/main --hard > /dev/null
 source env/bin/activate
 pip3 install -r requirements.txt > /dev/null
 systemctl daemon-reload
 systemctl restart myportfolio
 deactivate
-cd ..
