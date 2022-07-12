b5466579cc003ea49f487cac6188d4accb7d0aad
talikeb <talikeb20@gmail.com>
2022-07-11 21:44:55 -0400

    uploaded redeploy_site.sh

--- redeploy_site.sh
+++ redeploy_site.sh
@@ -2,6 +2,6 @@
 
 cd MLH-portfolio-site
 git fetch && git reset origin/main --hard > /dev/null
-docker compose -f docker-compose.prod.yml down > /dev/null
-docker compose -f docker-compose.prod.yml up -d --build > /dev/null
+docker compose -f docker_compose.prod.yml down > /dev/null
+docker compose -f docker_compose.prod.yml up -d --build > /dev/null
 cp redeploy_site.sh ..
