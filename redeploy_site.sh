5b80d73e11378bb5dc6dfaf8817f31c7e5b13b93
talikeb <talikeb20@gmail.com>
2022-08-18 17:49:22 -0400

    MAJOR CHANGE: restructured project for Heroku - removed user_conf.d/ -> no longer needed since NGINX container is not being used - removed redeploy_site.sh -> no longer needed since deployment is done automatically by Heroku - removed example.env -> not necessary - removed Dockerfile -> no longer needed since deployment is done automatically by Heroku - removed docker_compose.yml and docker_compose.prod.yml -> no longer needed since deployment is done automatically by Heroku - commented out Configure SSH and Deploy Site steps in deploy.yml -> no longer needed since VPS is not being used - created Procfile -> required to start up Heroku deployment - moved content out of app/ -> Python file needs to be detected by Procfile

--- redeploy_site.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-cd MLH-portfolio-site
-git fetch && git reset origin/main --hard
-docker compose -f docker_compose.prod.yml down
-docker compose -f docker_compose.prod.yml up -d --build
-cp redeploy_site.sh ..
