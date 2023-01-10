bf7b244f424d85f41b3441496df4bfa0471936a1
talikeb <talikeb20@gmail.com>
2023-01-10 14:11:00 -0500

    initiating attempt to re-host website - removed database

--- __init__.py
+++ __init__.py
@@ -15,35 +15,35 @@ from playhouse.shortcuts import model_to_dict # model_to_dict()
 # Connect local MySQL database to Heroku database: https://www.youtube.com/watch?v=Zcg71lxW-Yo
 # Configuration variables: https://devcenter.heroku.com/articles/config-vars
 
-load_dotenv() # Loads .env file; the data from MySQL is read
+# load_dotenv() # Loads .env file; the data from MySQL is read
 app = Flask(__name__)
 
-# Connect to database depending on environment variable
-if os.getenv("TESTING") == "true":
-    # Establish copy of app in memory (?)
-    print("Running in test mode")
-    mydb = SqliteDatabase('file:memory?mode=memory&cache=shared', uri=True)
-else: 
-    mydb = MySQLDatabase(os.getenv("MYSQL_DATABASE"),
-        user=os.getenv("MYSQL_USER"),
-        password=os.getenv("MYSQL_PASSWORD"),
-        host=os.getenv("MYSQL_HOST"),
-        port=3306)
+# # Connect to database depending on environment variable
+# if os.getenv("TESTING") == "true":
+#     # Establish copy of app in memory (?)
+#     print("Running in test mode")
+#     mydb = SqliteDatabase('file:memory?mode=memory&cache=shared', uri=True)
+# else: 
+#     mydb = MySQLDatabase(os.getenv("MYSQL_DATABASE"),
+#         user=os.getenv("MYSQL_USER"),
+#         password=os.getenv("MYSQL_PASSWORD"),
+#         host=os.getenv("MYSQL_HOST"),
+#         port=3306)
 
-# Object-relational mapper (ORM) model (?)
-class TimelinePost(Model):
-    name = CharField()
-    email = CharField()
-    content = TextField()
-    created_at = DateTimeField(default=datetime.datetime.now)
+# # Object-relational mapper (ORM) model (?)
+# class TimelinePost(Model):
+#     name = CharField()
+#     email = CharField()
+#     content = TextField()
+#     created_at = DateTimeField(default=datetime.datetime.now)
 
-    class Meta:
-        database = mydb
+#     class Meta:
+#         database = mydb
 
-mydb.connect()
-mydb.create_tables([TimelinePost])
-if os.getenv("TESTING") != "true":
-    mydb.close()
+# mydb.connect()
+# mydb.create_tables([TimelinePost])
+# if os.getenv("TESTING") != "true":
+#     mydb.close()
 
 # WEBSITE PAGES ##########
 # Home page
@@ -73,58 +73,58 @@ def timeline():
 ########################################
 
 # HTTP requests + database interaction ##########
-# POST request: Adds timeline post to database
-@app.route("/api/timeline-post/", methods=['POST'])
-def post_timeline_post():
-    # SOURCE: https://stackoverflow.com/questions/57664997/how-to-return-400-bad-request-on-flask
-    if request.form.get('name') == None or request.form.get('name') == "":
-        return Response(
-            "Invalid name.",
-            status=400
-        )
-    elif request.form.get('content') == "":
-        return Response(
-            "Invalid content.",
-            status=400
-        )
-    elif not('@' in request.form.get('email')):
-        return Response(
-            "Invalid email.",
-            status=400
-        )
-    name = request.form['name']
-    email = request.form['email']
-    content = request.form['content']
-    timeline_post = TimelinePost.create(name=name, email=email, content=content)
-    if os.getenv("TESTING") != "true":
-        mydb.close()
-    return model_to_dict(timeline_post)
+# # POST request: Adds timeline post to database
+# @app.route("/api/timeline-post/", methods=['POST'])
+# def post_timeline_post():
+#     # SOURCE: https://stackoverflow.com/questions/57664997/how-to-return-400-bad-request-on-flask
+#     if request.form.get('name') == None or request.form.get('name') == "":
+#         return Response(
+#             "Invalid name.",
+#             status=400
+#         )
+#     elif request.form.get('content') == "":
+#         return Response(
+#             "Invalid content.",
+#             status=400
+#         )
+#     elif not('@' in request.form.get('email')):
+#         return Response(
+#             "Invalid email.",
+#             status=400
+#         )
+#     name = request.form['name']
+#     email = request.form['email']
+#     content = request.form['content']
+#     timeline_post = TimelinePost.create(name=name, email=email, content=content)
+#     if os.getenv("TESTING") != "true":
+#         mydb.close()
+#     return model_to_dict(timeline_post)
 
-# GET request: Puts posts in descending order
-@app.route("/api/timeline-post/", methods=['GET'])
-def get_timeline_post():
-    api = {'timeline_posts':[model_to_dict(p) for p in TimelinePost.select().order_by(TimelinePost.created_at.desc())]}
-    response = jsonify(api)
-    response.headers.add('Access-Control-Allow-Origin', '*')
-    if os.getenv("TESTING") != "true":
-        mydb.close()
-    return response
+# # GET request: Puts posts in descending order
+# @app.route("/api/timeline-post/", methods=['GET'])
+# def get_timeline_post():
+#     api = {'timeline_posts':[model_to_dict(p) for p in TimelinePost.select().order_by(TimelinePost.created_at.desc())]}
+#     response = jsonify(api)
+#     response.headers.add('Access-Control-Allow-Origin', '*')
+#     if os.getenv("TESTING") != "true":
+#         mydb.close()
+#     return response
 
-# DELETE request: Deletes a specific post
-@app.route("/api/timeline-post/<list_num>/", methods=['DELETE'])
-def delete_timeline_post(list_num):
-    # SOURCE: https://docs.peewee-orm.com/en/latest/peewee/querying.html
-    data = TimelinePost.select().order_by(TimelinePost.created_at.desc())
-    counter = 1
-    for post in data:
-        if counter == int(list_num):
-            post.delete_instance()
-            break
-        else:
-            counter += 1
-    if os.getenv("TESTING") != "true":
-        mydb.close()
-    return 'ok'
+# # DELETE request: Deletes a specific post
+# @app.route("/api/timeline-post/<list_num>/", methods=['DELETE'])
+# def delete_timeline_post(list_num):
+#     # SOURCE: https://docs.peewee-orm.com/en/latest/peewee/querying.html
+#     data = TimelinePost.select().order_by(TimelinePost.created_at.desc())
+#     counter = 1
+#     for post in data:
+#         if counter == int(list_num):
+#             post.delete_instance()
+#             break
+#         else:
+#             counter += 1
+#     if os.getenv("TESTING") != "true":
+#         mydb.close()
+#     return 'ok'
 ########################################
 
 if __name__ == "__main__":
